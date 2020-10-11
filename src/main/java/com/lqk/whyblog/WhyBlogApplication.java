baab8e5b91b3f0fa9291d51db54b327e0d714603
刘乾坤 <Universe_Liu@163.com>
2020-10-11 16:04:55 +0800

    新建

---
+++ src/main/java/com/lqk/whyblog/WhyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.lqk.whyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhyBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lqk/whyblog/WhyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lqk.whyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
